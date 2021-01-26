571fea2aa9eb07310965eeeb8efbfcda64bc47dc
philkazan <phillip.kazanjian@accolade.com>
2021-01-26 11:43:13 -0500

    config webpack

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello TypeScript!');
+import { callbackify } from "util";
+
+const input = 'fooBar';
+
+function doSomethingWithTheCallBack(callback) {
+    callback();
+}
+
+// anonymous function
+(input) => {
+    console.log(input);
+}
+
+
+doSomethingWithTheCallBack((input) => {
+    console.log(input);
+});
